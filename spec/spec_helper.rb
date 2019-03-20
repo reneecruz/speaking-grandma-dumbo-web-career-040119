7a1237aeb9a4c7345ef3932851d0782ddc961ded
Renée Cruz <reneebasscruz@gmail.com>
2019-03-20 01:19:53 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@
 # Require this file using `require "spec_helper"` to ensure that it is only
 # loaded once.
 require_relative '../grandma.rb'
-end
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # config.treat_symbols_as_metadata_keys_with_true_values = true
